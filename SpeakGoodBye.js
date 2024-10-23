dca8e5c11bb250a6595d4495370ab33f7f73dfb9
DaveGeorgeDesign <dave@davegeorgedesign.co.uk>
2024-10-23 13:41:30 +0100

    Update SpeakGoodBye.js

--- SpeakGoodBye.js
+++ SpeakGoodBye.js
@@ -1,9 +1,17 @@
 (function(window) { 
+
   const speakWord = "Good Bye";
+
   const byeSpeaker = {};  
+
   byeSpeaker.speak = function (name) {
+    
     console.log(speakWord + " " + name);
+    
     document.write(speakWord + " " + name + "<br>");
+  
   }
+  
   window.byeSpeaker =  byeSpeaker;
+
 })(window);
